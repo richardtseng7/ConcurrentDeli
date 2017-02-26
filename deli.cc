f18da56bb84acbdf5c52b9575d29b5af74da277d
Wendy Lau <wcl7@duke.edu>
2017-02-25 21:49:17 -0500

    did I fix git

--- deli.cc
+++ deli.cc
@@ -17,15 +17,17 @@ struct orders {
 	int cashierNum;
 };
 
-std::vector<orders> corkboard;
+int cashiers_alive;
+int lastOrder = -1;
 int max_orders;
-int numCashier; 
+
+int waiters = 0;
+
+std::vector<orders> corkboard;
 std::map<int, std::vector<int> > cashierMap; 
 
-unsigned int hasRoom = 2;
-unsigned int isFull = 3;
-int lastOrder = - 1;
-int waiters = 0;
+unsigned int hasRoom = 2000;
+unsigned int isFull = 3000;
 
 // helper method checks if cashier has posted on corkboard
 bool ifContains(int cashierID) {
@@ -44,25 +46,21 @@ bool ifContains(int cashierID) {
 
 
 void sandwichMaker(void* arg){
-	thread_lock(1);
-	while (numCashier != 0){
-		//DEBUG
-		printf("sandwichMaker \n");
-		for (int i = 0; i < corkboard.size(); i++){
-			printf("Cashier %d: Sandwich %d, \n", corkboard[i].cashierNum, corkboard[i].sandwichNum);
-		}
-
-		// while there is still room in corkboard, let cashier add orders
-		while(((int) corkboard.size()) < max_orders){ 
-			thread_broadcast(1, hasRoom);
+	//thread_lock(1);
+	while ((corkboard.size() > 0) || (cashiers_alive > 0)){	
+		thread_lock(1);
+		while(corkboard.size() < min(cashiers_alive, max_orders)){ 
 			thread_wait(1, isFull);
 		}
-		// find most similar sandwich to make
+		if (cashiers_alive == 0){
+			thread_unlock(1);
+			return;
+		}
 		int min = INT_MAX;
 		int ind;
 		int currCashier;
 		int currSandwich;
-		for (int i = 0; i < ((int) corkboard.size()); i++){
+		for (int i = 0; i < corkboard.size(); i++){
 			int diff = abs(lastOrder - corkboard[i].sandwichNum);
 			if (diff < min){ 
 				min = diff;
@@ -72,80 +70,57 @@ void sandwichMaker(void* arg){
 			}
 		}
 		lastOrder = currSandwich;
-		thread_broadcast(1, hasRoom);
-		if (waiters > 0){
-			waiters = waiters - 1;
-			thread_signal(1, (currCashier + 100));
-		}
-		//print currSandwich number & currCashierID
-		cout << "READY: cashier " << currCashier << " sandwich " << currSandwich << endl; 
-		//remove last sandwich order from corkboard
+		cout << "READY: cashier " << currCashier << " sandwich " << currSandwich << endl;
 		corkboard.erase(corkboard.begin() + ind);
-		
+		if (cashierMap[currCashier].size() == 0){
+			cashiers_alive--;
+		}
+		thread_broadcast(1, hasRoom);
+		thread_unlock(1);
 	}	
-	thread_unlock(1);
+	//thread_unlock(1);
 }
 
 
-void cashiers(void* cashierid){
-	thread_lock(1);	
-	int cashierID = (intptr_t) cashierid;
-	//while there are still orders
+void cashiers(void* arg){
+	//thread_lock(1);
+	int cashierID = (intptr_t) arg;
 	while (cashierMap[cashierID].size() > 0){
-	
-		// cashier already has an order on corkboard
-		while (ifContains((intptr_t) cashierID)) {
-			waiters++;
-			thread_wait(1, (cashierID + 100));
-		}
-		while (((int)corkboard.size()) == max_orders){
-			thread_signal(1, isFull);
+		thread_lock(1);
+		// cashier CANNOT post on corkboard, wait on these 2 conditions
+		while ((corkboard.size() == max_orders) || (ifContains(cashierID))) {
 			thread_wait(1, hasRoom);
 		}
-		if (numCashier < max_orders){
-			max_orders = numCashier;
-		}	
-
-		printf("corkboard size: %d, max_orders: %d", (int) corkboard.size(), max_orders);
-		printf("Cashier %d:", cashierID);
-		for(int i = 0; i < cashierMap[cashierID].size(); i++){
-			printf("%d,", cashierMap[cashierID][i]);
+		// cashier can post on corkboard, proceed
+		if (cashierMap[cashierID].size() > 0){
+			orders toAdd;
+			toAdd.cashierNum = cashierID;
+			toAdd.sandwichNum = cashierMap[cashierID][0];
+			corkboard.push_back(toAdd);
+			cashierMap[cashierID].erase(cashierMap[cashierID].begin());
+			cout << "POSTED: cashier " << toAdd.cashierNum << " sandwich " << toAdd.sandwichNum << endl;
 		}
-		printf("\n");
-		// add order from cashierMap to corkboard
-		orders toAdd;
-		toAdd.cashierNum = cashierID;
-		toAdd.sandwichNum = cashierMap[cashierID][0];
-		corkboard.push_back(toAdd);
-		cashierMap[cashierID].erase(cashierMap[cashierID].begin());
-
-		// print sandwich order
-		cout << "POSTED: cashier " << toAdd.cashierNum << " sandwich " << toAdd.sandwichNum << endl; 
-	}
-	numCashier = numCashier - 1;
-	if (corkboard.size() > max_orders){
 		thread_signal(1, isFull);
+		thread_unlock(1);
 	}
-	thread_unlock(1);
+	//thread_unlock(1);
 }
 
 
 void cashierInit(void* arg){
-	// initialize all cashiers threads
-	for (int i = 0; i < numCashier; i++){ 
-		thread_create((thread_startfunc_t) cashiers, (void *) (intptr_t) i);
-	}
 	// initialize sandwich maker thread
 	thread_create((thread_startfunc_t) sandwichMaker, (void *) 14);
-
+	// initialize all cashiers threads
+	for (int i = 0; i < cashiers_alive; i++){ 
+		thread_create((thread_startfunc_t) cashiers, (void *) (intptr_t) i);
+	}
 }
 
 
 int main(int argc, char* argv[]) {
-	//std::vector<orders> corkboard;
 	max_orders = atoi(argv[1]);
-	numCashier = argc - 2;
-	if (numCashier == 0) {
+	cashiers_alive = argc - 2;
+	if (cashiers_alive == 0) {
 		return 0;
 	}
 	//add sandwich orders to map here
